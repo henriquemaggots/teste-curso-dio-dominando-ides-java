432e136200729749fda0093161349d8ac1a0caa3
henriquemaggots <henriquemds@gmail.com>
2022-02-06 21:28:26 -0300

    Meu primeiro programa Java

---
+++ primeiro programa java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros Livros = new Livros();
+		//System.out.println("Hello World!!! ");
+		
+		System.out.println(gato);
+		System.out.println(Livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npagString;
+}
